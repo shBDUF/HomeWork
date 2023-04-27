ca10755e3c2de502cb838eef88a270a6baad2e21
Grigoriy <inpuct@mail.ru>
2023-04-27 10:21:17 +0300

    First Ex

--- Program.cs
+++ Program.cs
@@ -1,1 +1,11 @@
-﻿
+﻿// int a, b;
+// a = Convert.ToInt32(Console.ReadLine());
+// b = Convert.ToInt32(Console.ReadLine());
+// if (a>b)
+// {
+//     Console.WriteLine($"число {a} больше числа {b}");
+// }
+// else 
+// {
+//     Console.WriteLine($"число {b} больше числа {a}");
+// }
