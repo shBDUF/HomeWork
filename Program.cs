72c44e21ee9162cf0af44d5bedcd8e90a1f8704e
Grigoriy <inpuct@mail.ru>
2023-04-27 10:28:38 +0300

    Second Ex

--- Program.cs
+++ Program.cs
@@ -9,3 +9,18 @@
 // {
 //     Console.WriteLine($"число {b} больше числа {a}");
 // }
+
+int firstNum, secondNum, thirdNum;
+firstNum = Convert.ToInt32(Console.ReadLine());
+secondNum = Convert.ToInt32(Console.ReadLine());
+thirdNum = Convert.ToInt32(Console.ReadLine());
+int max = firstNum;
+if(secondNum>max)
+{
+    max= secondNum;
+}
+if (thirdNum>max)
+{
+    max = thirdNum;
+}
+Console.WriteLine($"Максимальное число из трёх это {max}");
