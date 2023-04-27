31623e2a071a5904c4e3b762c5db13155fe78364
Grigoriy <inpuct@mail.ru>
2023-04-27 10:32:06 +0300

    Third Ex

--- Program.cs
+++ Program.cs
@@ -10,17 +10,28 @@
 //     Console.WriteLine($"число {b} больше числа {a}");
 // }
 
-int firstNum, secondNum, thirdNum;
+// int firstNum, secondNum, thirdNum;
+// firstNum = Convert.ToInt32(Console.ReadLine());
+// secondNum = Convert.ToInt32(Console.ReadLine());
+// thirdNum = Convert.ToInt32(Console.ReadLine());
+// int max = firstNum;
+// if(secondNum>max)
+// {
+//     max= secondNum;
+// }
+// if (thirdNum>max)
+// {
+//     max = thirdNum;
+// }
+// Console.WriteLine($"Максимальное число из трёх это {max}");
+
+int firstNum;
 firstNum = Convert.ToInt32(Console.ReadLine());
-secondNum = Convert.ToInt32(Console.ReadLine());
-thirdNum = Convert.ToInt32(Console.ReadLine());
-int max = firstNum;
-if(secondNum>max)
+if(firstNum%2==0)
 {
-    max= secondNum;
+    Console.WriteLine($"Число {firstNum} делится на два без остатка");
 }
-if (thirdNum>max)
+else 
 {
-    max = thirdNum;
+    Console.WriteLine($"Число {firstNum} не делится на два без остатка");
 }
-Console.WriteLine($"Максимальное число из трёх это {max}");
